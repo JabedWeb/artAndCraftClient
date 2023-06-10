46485a90def62caa8086699e70aaf77ee48611f1
jabed366 <jabedhasan231@gmail.com>
2023-06-10 15:48:53 +0600

    Tankstack query for users

--- src/hooks/UserUser.jsx
+++ src/hooks/UserUser.jsx
@@ -2,13 +2,10 @@ import {useQuery} from '@tanstack/react-query'
 
 const UseUser =() => {
     
-        const {data:ReUser=[],isLoading,refetch}=useQuery({
-            queryKey:['ReUser'],
-            queryFn: async () => {
+        const {data:ReUser=[],isLoading,refetch}=useQuery(['ReUser'], async () => {
                 const response = await fetch(`http://localhost:5000/users`);
                 const data = await response.json();
                 return data;
-            }
         })
         return [ReUser,isLoading,refetch]
 
