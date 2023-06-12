dac1593ca3eb44ec787ce4c0020ee3204909d83f
jabed366 <jabedhasan231@gmail.com>
2023-06-12 07:30:40 +0600

    Environment variable to env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,15 +2,14 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAA6DObDg0f60fNp9BJ-ru0IprmndPVkWM",
-  authDomain: "artandcraft-5a453.firebaseapp.com",
-  projectId: "artandcraft-5a453",
-  storageBucket: "artandcraft-5a453.appspot.com",
-  messagingSenderId: "222758002598",
-  appId: "1:222758002598:web:e26f265965f72188cdb967"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
